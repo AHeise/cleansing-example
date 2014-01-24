443ea9c1b7c74f9b63b955b07dc32750a4e0fe36
Fabian Tschirschnitz <fabian.tschirschnitz@student.hpi.uni-potsdam.de>
2014-01-24 13:10:19 +0100

    added dummy script

--- examples/src/test/java/eu/stratosphere/sopremo/cleansing/examples/ScriptRunner.java
+++ examples/src/test/java/eu/stratosphere/sopremo/cleansing/examples/ScriptRunner.java
@@ -1,22 +1,84 @@
 package eu.stratosphere.sopremo.cleansing.examples;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
-import eu.stratosphere.meteor.MeteorIT;
+import eu.stratosphere.meteor.QueryParser;
+import eu.stratosphere.sopremo.client.DefaultClient;
 import eu.stratosphere.sopremo.operator.SopremoPlan;
+import eu.stratosphere.sopremo.query.QueryParserException;
+import eu.stratosphere.sopremo.server.SopremoTestServer;
 
-public class ScriptRunner extends MeteorIT{
+public class ScriptRunner{
+	protected SopremoTestServer testServer;
+
+	protected DefaultClient client;
+
+	protected File inputDir;
+	
 	@Test
 	public void testSuccessfulExecution() throws IOException {
-		final SopremoPlan plan = parseScript(new File("src/test/resources/ScrubbingIT.script"));
+		File scriptFile = new File("src/main/meteor/dummy.script");
+		final SopremoPlan plan = parseScript(scriptFile);
 
 		this.client.submit(plan, null, true);
-		
-		//IJsonNode[] nodeArray = getContentsToCheckFrom("src/test/resources/TestOutput.json");
-		
-		//this.testServer.checkContentsOf("TestOutput.json", nodeArray);
+	}
+
+	@Before
+	public final void setup() throws Exception {
+		this.testServer = new SopremoTestServer(true);
+		this.inputDir = this.testServer.createDir("input");
+
+		this.client = new DefaultClient();
+		this.client.setServerAddress(this.testServer.getServerAddress());
+		this.client.setUpdateTime(100);
+	}
+
+	@After
+	public void teardown() throws Exception {
+		this.client.close();
+		this.testServer.close();
+	}
+	
+	public SopremoPlan parseScript(final File script) {
+		// printBeamerSlide(script);
+		SopremoPlan plan = null;
+		try {
+			final QueryParser queryParser = new QueryParser().withInputDirectory(script.getParentFile());
+			
+			plan = queryParser.tryParse(this.loadScriptFromFile(script));
+		} catch (final QueryParserException e) {
+			final AssertionError error =
+				new AssertionError(String.format("could not parse script: %s", e.getMessage()));
+			error.initCause(e);
+			throw error;
+		}
+
+		Assert.assertNotNull("could not parse script", plan);
+
+		// System.out.println(plan);
+		return plan;
+	}
+	
+	private String loadScriptFromFile(final File scriptFile) {
+		try {
+			final BufferedReader reader = new BufferedReader(new FileReader(scriptFile));
+			final StringBuilder builder = new StringBuilder();
+			int ch;
+			while ((ch = reader.read()) != -1)
+				builder.append((char) ch);
+			reader.close();
+			return builder.toString();
+		} catch (final IOException e) {
+			throw new RuntimeException(e);
+		}
+
 	}
 }
