65774ca0d31f4d368967b5ab8bce82447eb5681d
Fabian Tschirschnitz <fabian.tschirschnitz@student.hpi.uni-potsdam.de>
2014-01-24 11:39:44 +0100

    added ScriptRunner.java

--- examples/src/main/java/eu/stratosphere/sopremo/cleansing/examples/ScriptRunner.java
+++
@@ -1,5 +0,0 @@
-package eu.stratosphere.sopremo.cleansing.examples;
-
-public class ScriptRunner {
-
-}
---
+++ examples/src/test/java/eu/stratosphere/sopremo/cleansing/examples/ScriptRunner.java
@@ -0,0 +1,22 @@
+package eu.stratosphere.sopremo.cleansing.examples;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Test;
+
+import eu.stratosphere.meteor.MeteorIT;
+import eu.stratosphere.sopremo.operator.SopremoPlan;
+
+public class ScriptRunner extends MeteorIT{
+	@Test
+	public void testSuccessfulExecution() throws IOException {
+		final SopremoPlan plan = parseScript(new File("src/test/resources/ScrubbingIT.script"));
+
+		this.client.submit(plan, null, true);
+		
+		//IJsonNode[] nodeArray = getContentsToCheckFrom("src/test/resources/TestOutput.json");
+		
+		//this.testServer.checkContentsOf("TestOutput.json", nodeArray);
+	}
+}
