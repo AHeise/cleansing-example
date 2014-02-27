85992999dd7b5171752c0eacf820eeb2eda314c2
Tommy Neubert <tommy.neubert@student.hpi.uni-potsdam.de>
2014-02-28 00:18:33 +0100

    changed person scripts to tag entities with source (scripts propably not executable at the moment)

--- examples/src/test/java/eu/stratosphere/sopremo/cleansing/examples/ScriptRunner.java
+++ examples/src/test/java/eu/stratosphere/sopremo/cleansing/examples/ScriptRunner.java
@@ -30,7 +30,7 @@ public class ScriptRunner{
 		File scriptFile3 = new File("src/main/meteor/Freebase-Politicians.script");
 		File scriptFile4 = new File("src/main/meteor/record_linkage_persons.script");
 		File scriptFile5 = new File("src/main/meteor/Freebase-Company.script");
-		final SopremoPlan plan = parseScript(scriptFile4);
+		final SopremoPlan plan = parseScript(scriptFile3);
 
 		this.client.submit(plan, null, true);
 	}
